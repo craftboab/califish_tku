30b160ea8b173fb1f1e252381001073fbd5427c7
craftboab <craftboab@gmail.com>
2021-08-08 20:24:19 -0700

    forth

--- boab/settings.py
+++ boab/settings.py
@@ -78,13 +78,24 @@ WSGI_APPLICATION = 'boab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'califish_tku',
+        'USER': 'califish-tku',
+        'PASSWORD': 'boabadmin',
+        'HOST': '127.0.0.1',
+        'PROT': 3306
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
